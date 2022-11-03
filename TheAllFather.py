b54631598bfbe8d3a5916b228d2c189b8324c802
Henry Karn <bobbyboy4020@gmail.com>
2022-11-03 16:07:09 -0400

    added FRB sort read, while True checks, better image use, restructured some Retake code, sorts ask redone, removed Originaling for now, was unused, removed new jobCodes and Originaling choices

--- TheAllFather.py
+++ TheAllFather.py
@@ -5,6 +5,9 @@ Settings.MoveMouseDelay = 0.0   #mouse instant movement
 reportSaveLoc = r"C:\Users\hkarn\Documents\PDF\FreshReports\Nov 1 - Nov 18"
 jobCodes = []
 choice = ""
+sortArray = []
+frbDone = False
+
 
 def CopyClip():
 	type('c', KeyModifier.CTRL)
@@ -165,6 +168,11 @@ def FreshReporting(jobCodes):
 			frbReg.click("HPC0printer.png")
 		frbReg.click(Pattern("Printallpage.png").targetOffset(-9,21))
 		sleep(2)
+
+		Screen(1).doubleClick(Pattern("HomeroomSpot.png").similar(0.65).targetOffset(0,-15))
+		sort = CopyClip()
+		sortArray.append(sort[0])
+		
 	def FreshReportMulti(jobCodes): #to print multiple reports at the same time
 		frbApp = []
 		first = True
@@ -284,29 +292,49 @@ def Retaking(jobCodes):
 		eeReg.click(Pattern("EXE-SecondCheck.png").targetOffset(22,68))#clicks ok
 		sleep(0.5)
 		eeReg.click("FindSubjectEE.png")
+		while True:
+			if(eeReg.exists("ImageLoadedEE.png")):
+				break
+			else:
+				eeReg.click("FindSubjectEE.png")
 		sleep(0.1)
 		eeReg.click(Pattern("ImageLoadedEE.png").targetOffset(24,6))
 		sleep(0.1)
 		eeReg.click("FindSubjectButton.png")
 		sleep(0.5)
-		eeReg.click("SortButtonEE.png")         #sort job before exe making
-		eeReg.wait("SortingSquare.png")
-		sleep(0.1)
-		if ("h" in sortType):
-			eeReg.click("SortingSquare.png")
-		elif ("g" in sortType):
-			eeReg.click(Pattern("SortingSquare.png").targetOffset(0,-31))
-		elif ("a" in sortType):
-			eeReg.click(Pattern("SortingSquare.png").targetOffset(0,31))
+		eeReg.click("SortButtonEE.png")
+		while True:
+			if(eeReg.exists("quickSort.png")):
+				break
+			else:
+				eeReg.click("SortButtonEE.png")
+				sleep(0.2)
+		sleep(0.2)
+		if not (sortType == ""):
+			sortType = sortType[0]
+			sortType.lower()
+		if ('h' in sortType):
+			eeReg.click("quickSort.png")
+		elif ('g' in sortType):
+			eeReg.click(Pattern("quickSort.png").targetOffset(0,-27))
+		elif ('a' in sortType):
+			eeReg.click(Pattern("quickSort.png").targetOffset(0,35))
 		else:
-			if(codes[1]=="E231"):
-				eeReg.click("SortingSquare.png")
-			elif(codes[1]=="H231"):
-				eeReg.click(Pattern("SortingSquare.png").targetOffset(0,-32))
-			elif(codes[1]=="E232"):
-				eeReg.click("SortingSquare.png")
+			if(codes[1] == "E231"):
+				eeReg.click("quickSort.png")
+			elif(codes[1] == "H231"):
+				eeReg.click(Pattern("quickSort.png").targetOffset(0,-27))
+			elif(codes[1] == "E232"):
+				eeReg.click("quickSort.png")
+			elif(codes[1] == "H232"):
+				eeReg.click(Pattern("quickSort.png").targetOffset(0,-27))
 		sleep(0.5)
 		eeReg.click("ReportsPage1.png")
+		while True:
+			if(eeReg.exists("ReportsName.png")):
+				break
+			else:
+				eeReg.click("ReportsPage1.png")
 		sleep(0.25)
 		eeReg.click(Pattern("ReportsName.png").targetOffset(154,39))
 		sleep(0.2)
@@ -349,7 +377,7 @@ def Retaking(jobCodes):
 		regon = Region(0,0,150,150)
 		setAutoWaitTimeout(10)
 		try:
-			regon.wait("ADOBEprintCCards.png")#Wait for the camera cards to start printing
+			regon.wait("ADOBEprintCCards.png")			#Wait for the camera cards to start printing
 			while regon.exists("ADOBEprintCCards.png"): #while printing exists, stay printing
 				sleep(1)
 		except:
@@ -365,7 +393,6 @@ def Retaking(jobCodes):
 		#we run through the exe maker 3 times first after that, 
 		#go to the first job started and wait for the confirmation window either do cards for job that finished exe, 
 		#or wait for other 2 to finish so no popups mess with the run
-		
 		eeApp = [] #array for Edge Entry Apps
 		codes = [] #array for split job codes
 		apps = ""
@@ -398,19 +425,27 @@ def Retaking(jobCodes):
 			exeGood = ""
 			try:
 				exeMake = popAsk("Make exes? " + str(jobCodes[i]) + ", " + str(jobCodes[i+1]) + ", " + str(jobCodes[i+2]))
-				sorts = input("sorts for jobs? split by comma, enter nothing if all are default\nh for homeroom, g for grade, a for alpha\n" + str(jobCodes[i]) + ", " + str(jobCodes[i+1]) + ", " + str(jobCodes[i+2]))
+				if not frbDone:		
+					sorts = input("sorts for jobs? split by comma, enter nothing if all are default\nh for homeroom, g for grade, a for alpha\n" + str(jobCodes[i]) + ", " + str(jobCodes[i+1]) + ", " + str(jobCodes[i+2]))
+					sorts = sorts.split(",") #h for homeroom, g for grade, a for alpha
 			except:
 				try:
 					exeMake = popAsk("Make exes? " + str(jobCodes[i]) + ", " + str(jobCodes[i+1]))
-					sorts = input("sorts for jobs? split by comma, enter nothing if all are default\nh for homeroom, g for grade, a for alpha\n" + str(jobCodes[i]) + ", " + str(jobCodes[i+1]))
+					if not frbDone:
+						sorts = input("sorts for jobs? split by comma, enter nothing if all are default\nh for homeroom, g for grade, a for alpha\n" + str(jobCodes[i]) + ", " + str(jobCodes[i+1]))
+						sorts = sorts.split(",") #h for homeroom, g for grade, a for alpha
 				except:
 					try:
 						exeMake = popAsk("Make exes? " + str(jobCodes[i]))
-						sorts = input("sorts for jobs? split by comma, enter nothing if all are default\nh for homeroom, g for grade, a for alpha\n" + str(jobCodes[i]))
+						if not frbDone:
+							sorts = input("sorts for jobs? split by comma, enter nothing if all are default\nh for homeroom, g for grade, a for alpha\n" + str(jobCodes[i]))
+							sorts = sorts.split(",") #h for homeroom, g for grade, a for alpha
 					except:
 						exeMake = popAsk("Make exes?")
-						sorts = input("sorts for jobs? split by comma, enter nothing if all are default\nh for homeroom, g for grade, a for alpha\n")
-			sorts = sorts.split(",") #h for homeroom, g for grade, a for alpha
+						if not frbDone:
+							sorts = input("sorts for jobs? split by comma, enter nothing if all are default\nh for homeroom, g for grade, a for alpha\n")
+							sorts = sorts.split(",") #h for homeroom, g for grade, a for alpha
+			
 			if(exeMake):
 				CreateJobRetake(codes[i], eeApp[0], eeReg)
 				MakeExe(codes[i], eeApp[0])
@@ -434,16 +469,25 @@ def Retaking(jobCodes):
 			#goto job 1 and wait for confirmation window
 			exeGood = popAsk("Exe made?")
 			if(exeGood):
-				SaveCardsRetake(codes[i], eeApp[0], True, eeReg, sorts[0])
+				if(frbDone): #if reports have been built, will have sort, wont have to ask user
+					SaveCardsRetake(codes[i], eeApp[0], True, eeReg, sortArray[i])
+				else:
+					SaveCardsRetake(codes[i], eeApp[0], True, eeReg, sorts[0])
 				try:
-					SaveCardsRetake(codes[i+1], eeApp[1], False, eeReg, sorts[1])
+					if(frbDone):
+						SaveCardsRetake(codes[i+1], eeApp[1], False, eeReg, sortArray[i+1])
+					else:
+						SaveCardsRetake(codes[i+1], eeApp[1], False, eeReg, sorts[1])
 				except:
-					try:
+					try: #if prompt is left empty, sorts[1] and 2 won't work, defaults to blank
 						SaveCardsRetake(codes[i+1], eeApp[1], False, eeReg, "")
 					except:
 						pass
 				try:
-					SaveCardsRetake(codes[i+2], eeApp[2], False, eeReg, sorts[2])
+					if(frbDone):
+						SaveCardsRetake(codes[i+2], eeApp[2], False, eeReg, sortArray[i+2])
+					else:
+						SaveCardsRetake(codes[i+2], eeApp[2], False, eeReg, sorts[2])
 				except:
 					try:
 						SaveCardsRetake(codes[i+2], eeApp[2], False, eeReg, "")
@@ -478,310 +522,14 @@ def Retaking(jobCodes):
 			pass
 	Retaker(jobCodes)
 
-def Originaling(jobCodes):
-	def CreateJobOriginal(codes, eeApp, eeReg): #will create job if not already, and open email
-		eeApp.focus()
-		clickLoc = ""
-		sleep(1)
-		eeReg.click("JobsEE.png")
-		eeReg.click(Pattern("JobSelectEE.png").targetOffset(76,-14))
-		for i in range(3):
-			type(Key.BACKSPACE)
-		sleep(0.2)
-		paste(codes[0])
-		sleep(0.2)
-		type(Key.ENTER)
-		sleep(1)
-		eeReg.wait("ViewAssociation.png")
-		eeReg.click("ViewAssociation.png")
-		sleep(0.5)
-		clickLoc = jobFinder(codes[1])
-		doubleClick(clickLoc)
-		setAutoWaitTimeout(5)
-		if(eeReg.exists("CreateJobEE.png")):
-			eeReg.click("CreateJobEE.png")
-			sleep(2)
-		
-		try:
-			outApp = App(r"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE")
-			outApp.focus()
-			wait(Pattern("OutlookSearchBar.png").similar(0.62))
-			click(Pattern("OutlookSearchBar.png").similar(0.66).targetOffset(221,0))
-			for i in range(4):
-				type(Key.BACKSPACE)
-			sleep(0.1)
-			paste(codes[0])
-			sleep(0.1)
-			type(Key.ENTER)
-			sleep(1)		
-		except:
-			sleep(1)
-		eeApp.focus()
-		type('e', KeyModifier.CTRL)
-		wait("FileBrowserBarLeft.png")
-	def DatabaseTime(eeApp):
-		#Imports asciiclean into database onto subjects table of current job
-		ascii = ""
-		dataBase = ""
-		jobLoc = JobLocation()
-		sleep(0.1)
-		click("asciicleanCheck.png")
-		sleep(0.1)
-		type(Key.F2)
-		ascii = CopyClip()
-		sleep(0.2)
-		dataBase = jobLoc+"\\"+ascii+".xlsx"
-		doubleClick("DataFolderCheck.png")
-		wait("databaseEONE.png")
-		click("databaseEONE.png") #creates copy and renames copy to todays date
-		sleep(0.1)
-		rightClick("databaseEONE.png")
-		click(Pattern("databaseCopy.png").targetOffset(0,13))
-		sleep(0.25)
-		rightClick()
-		click(Pattern("databasePaste.png").targetOffset(-33,-10))
-		sleep(0.5)
-		type(Key.F2)
-		sleep(0.2)
-		today = date.today() #str is to string, zfill is to have int as always 2 characters, ex 05
-		paste("eone HRK " + str(today.month).zfill(2)+str(today.day).zfill(2))
-		sleep(0.2)
-		type(Key.ENTER)
-		sleep(0.3)
-		doubleClick("databaseEONE.png") #open database
-		sleep(0.5)
-		type(Key.LEFT)
-		sleep(0.5)
-		type(Key.ENTER)
-		sleep(0.2)
-		click("AccessExternalDataCheck.png")
-		sleep(0.2)
-		while True:
-			if(exists("AccessNewDataSource.png")):
-				break
-			else:
-				click("AccessExternalDataCheck.png") #start loading data segment
-		sleep(0.1)
-		click("AccessNewDataSource.png")
-		while True:
-			if(exists("databaseDataClick.png")):
-				break
-			else:
-				click("AccessNewDataSource.png") #start loading data segment
-		sleep(0.1)
-		type('f')
-		sleep(0.1)
-		type('x')
-		sleep(0.1)
-		type('a',KeyModifier.CTRL)
-		sleep(0.1)
-		paste(dataBase)  #location of asciiclean
-		sleep(0.1)
-		type(Key.TAB)
-		sleep(0.1)
-		type(Key.TAB)
-		sleep(0.1)
-		type(Key.DOWN)
-		sleep(0.1)
-		type(Key.TAB)
-		sleep(0.1) #maybe instead, type('v'), click on Subjects
-		type('s')
-		sleep(0.1)
-		for i in range(6):
-			type(Key.DOWN)
-			sleep(0.1)
-		sleep(0.4)
-		type(Key.ENTER)
-		sleep(0.1)
-		type(Key.TAB)
-		sleep(0.1)
-		type(Key.ENTER)
-		sleep(0.1)
-		setAutoWaitTimeout(20)
-		wait("ImportSpreadsheetWizard.png")
-		click("ImportSpreadsheetWizard.png")
-		for i in range(3):
-			type(Key.ENTER)
-			sleep(0.2)
-		sleep(1.5)
-		type(Key.ESC)
-		sleep(0.5)
-		type(Key.F4, KeyModifier.ALT) 
-		sleep(0.5)
-		click(Pattern("FileBrowserTopLeftCheck.png").targetOffset(720,-17))
-		sleep(0.5)
-	def SaveCardsOriginal(codes, eeApp, eeReg):
-		eeApp.focus()
-		sortType = input("what is sort type, gr, hr, al?")
-		sortType.lower()
-		eeReg.click("ViewAssociation.png")
-		sleep(0.2) #make sure 2023 job is selected
-		clickLoc = jobFinder(codes[1])
-		doubleClick(clickLoc)
-		sleep(0.5)
-		type('e', KeyModifier.CTRL)
-		jobLoc = JobLocation()
-		click(Pattern("FileBrowserTopLeftCheck.png").targetOffset(720,-17))
-		sleep(0.2) 
-		eeReg.click("SubjectsEE.png")
-		sleep(1.5)
-		
-		sleep(0.5)
-		regLoc = Location(find("File-Edit-EE.png").getTarget())
-		regExe = Region(regLoc.getX()+25,regLoc.getY()+5, 150,100)
-		keyDown(Key.ALT)
-		type('j')  
-		while True:
-			if(regExe.exists(Pattern("EXECheck.png").similar(0.76))):
-				break
-			else:
-				 keyDown(Key.ALT)
-				 type('j')     
-		sleep(0.15)
-		type('e')
-		sleep(0.15)
-		type('e')
-		sleep(0.15)
-		type('o')
-		sleep(0.15)
-		keyUp()
-		eeApp.focus()
-		setAutoWaitTimeout(600)  
-		if(codes[2]=="1"):
-			type(Key.ENTER)
-		while True:
-			if(eeReg.exists("EXE-FirstCheck.png")):
-				break
-			else:
-				sleep(1)
-		sleep(2)
-		setAutoWaitTimeout(30)
-		eeReg.click(Pattern("EXE-FirstCheck.png").targetOffset(2,51))#click ok
-		eeReg.wait("EXE-SecondCheck.png") 
-		sleep(2)
-		eeReg.click(Pattern("EXE-SecondCheck.png").targetOffset(22,68))#clicks ok
-		sleep(1)	
-		eeReg.click("SortButtonEE.png")         #sort job before exe making
-		sleep(0.5)
-		while True:
-			if(eeReg.exists("quickSort.png")):
-				break
-			else:
-				eeReg.click("SortButtonEE.png")  
-				sleep(0.2)
-		if ("h" in sortType):
-			eeReg.click("quickSort.png")
-		elif ("g" in sortType):
-			eeReg.click(Pattern("quickSort.png").targetOffset(0,-30))
-		elif ("a" in sortType):
-			eeReg.click(Pattern("quickSort.png").targetOffset(0,30))
-		else:
-			if(codes[1]=="E231"):
-				eeReg.click("quickSort.png")
-			elif(codes[1]=="H231"):
-				eeReg.click(Pattern("quickSort.png").targetOffset(0,-30))
-		sleep(0.5)
-		eeReg.click("ReportsPage1.png")
-		sleep(0.5)
-		eeReg.click(Pattern("ReportsName.png").targetOffset(154,39))
-		sleep(0.2)
-		paste("Q:\\Edge\\Lab\\Reports\\Camera Cards\\School\\ccard SAP (Standard).rtm") 
-		sleep(0.2)		#resets ccard SAP incase of ID last job
-		eeReg.click(Pattern("PrintCardType.png").targetOffset(53,-3))
-		sleep(0.1)
-		eeReg.click("StackOrderButton.png")       #Stack job and wait to preview
-		sleep(1)
-		type('v', KeyModifier.ALT)
-		sleep(1)
-		eeReg.wait("PrintPreviewCards.png")
-		sleep(1)
-		eeReg.click(Pattern("PrintPreviewCards.png").targetOffset(-37,27))
-		while True:
-			if(exists("OKButtonEE.png")):
-				break
-			else:
-				eeReg.click(Pattern("PrintPreviewCards-2.png").targetOffset(-37,27))	
-				sleep(0.2)
-		sleep(1)
-		eeReg.click("OKButtonEE.png")
-		sleep(1)
-		paste(jobLoc)
-		sleep(0.2)
-		type(Key.ENTER)
-		sleep(0.1)
-		type(Key.ENTER)
-		sleep(1)
-		regPrint = Region(0,0,150,150)
-		setAutoWaitTimeout(10)
-		try:
-			regPrint.wait("ADOBEprintCCards.png")#Wait for the camera cards to start printing
-			while regPrint.exists("ADOBEprintCCards.png"): #while printing exists, stay printing
-				sleep(1)
-		except:
-			sleep(1)
-		eeApp.focus()
-		setAutoWaitTimeout(5)  
-		sleep(1)
-		eeReg.click(Pattern("PrintPreviewCards.png").targetOffset(459,-1))
-		sleep(2)           
-		type('e', KeyModifier.CTRL)
-		sleep(2)
-	def PrintCards(codes):
-		Settings.MoveMouseDelay = 0.25
-		dragDrop(find("ccardPDF.png"),find(Pattern("CamerCardWhite.png").similar(0.80)))
-		Settings.MoveMouseDelay = 0
-		sleep(0.25)
-		click(Pattern("FileBrowserBar.png").targetOffset(-2,-1))
-		paste("C:\edge\jobs")
-		sleep(0.1)
-		type(Key.ENTER)
-		wait("DateMod-Type.png")
-		doubleClick(Pattern("DateMod-Type.png").targetOffset(-88,27)) #clicks most recently made exe and opens folder with it
-		doubleClick(Pattern("EXEDataFolder.png").targetOffset(-7,11))
-		sleep(0.2)
-		try:
-			Settings.MoveMouseDelay = 0.1
-			king = Location(find(Pattern("Kingston.png").similar(0.78)).getTarget())
-			dragDrop(find(Pattern("exeLogo.png").similar(0.84)),king)
-			Settings.MoveMouseDelay = 0
-			sleep(0.1)
-			click(king)
-			sleep(2) 
-		except:
-			popup("missing USB")
-		popup("Job done :)") #does PrintCards of current job
-	def OriginalJobs(jobCodes):
-		eeApp = App(r"Q:\Edge\Software\Edge Entry.exe")
-		eeReg = Region(390,115,1140,844) #region for Edge Entry Window
-		for job in jobCodes:
-			job.upper()
-			codes = job.split("-")
-			createOK = popAsk("Create Job? " + job) #does job need creating?
-			if(createOK):
-				CreateJobOriginal(codes, eeApp, eeReg)
-			popat(1500,100)
-			dataOK = popAsk("Is the ascii cleaned?") #does job need database filled?
-			if(dataOK):
-				DatabaseTime(eeApp)
-			popat() 
-			baseOK = popAsk("Database loaded?") #does job need cards and exe made?
-			if(baseOK):
-				SaveCardsOriginal(codes, eeApp, eeReg)
-			ccardOK = popAsk("Camera Cards OK? USB plugged in?") #does job need cards printed and exe to usb?
-			if(ccardOK):
-				PrintCards(codes)
-			popup("originals all done :^)")
-	OriginalJobs(jobCodes)
-
 jobCodes = CopyJobCodes()
 
-while not choice == "4":
-	choice = input("Enter Job Type: \n1 for Reports\n2 for Retakes\n3 for 0 Jobs\n4 to exit\n5 to get new jobcodes")
+while not (choice == "4"):
+	choice = input("Enter Job Type: \n1 for Reports\n2 for Retakes\n3 for 0 Jobs\n4 to exit")
 	if(choice == "1"):
 		FreshReporting(jobCodes)
+		frbDone = True
 	elif(choice == "2"):
 		Retaking(jobCodes)
-	elif(choice == "3"):
-		Originaling(jobCodes)
-	elif(choice == "5"):
-		jobCodes = CopyJobCodes()
+#	elif(choice == "3"):
+#		Originaling(jobCodes)
